4318f41de54a7215480b855fdf2ccb0ac4d52066
Brian Eggleston <bje301@gmail.com>
2017-10-15 23:44:48 -0400

    created relationship selector component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import RelationshipSelector from './selectionboxes';
+
+const relationships = ["mother", "father", "sister"]
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        First Name: <input type="text"/> <br />
+        Last  Name: <input type="text"/> <br />
+          Relationship : <RelationshipSelector />
+
+
       </div>
     );
   }
---
+++ src/selectionboxes/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by beggl on 10/15/2017.
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RelationshipSelector = ({relationships}) =>
+    <select>
+        { relationships.map( (r) =>
+            <option value ={r}> {r} </option>
+        )}
+    </select>
+
+RelationshipSelector.propTypes = {
+    relationships: PropTypes.arrayOf(PropTypes.string)
+
+}
+
+RelationshipSelector.defaultProps = {
+    relationships: ["Mother", "Father", "Sister", "Brother"]
+}
+
+export default RelationshipSelector
