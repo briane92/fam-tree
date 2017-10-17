e10570d2513ba71ed5c56f728ed0bee7f919cc17
Brian Eggleston <bje301@gmail.com>
2017-10-17 17:44:41 -0400

    add relationship button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import RelationshipSelector from './selectionboxes';
 
-const relationships = ["mother", "father", "sister"]
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <button type="button">Add Relationship</button>
+
+
         First Name: <input type="text"/> <br />
         Last  Name: <input type="text"/> <br />
           Relationship : <RelationshipSelector />
