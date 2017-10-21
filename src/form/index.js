3c0dc12522d76437ec3562b821f65c03c21710a9
Brian Eggleston <bje301@gmail.com>
2017-10-21 16:12:58 -0400

    refactored components to their own files

---
+++ src/form/index.js
@@ -0,0 +1,62 @@
+/**
+ * Created by beggl on 10/21/2017.
+ */
+import React, { Component } from 'react'
+
+class RelationshipForm extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            name: "Enter Name",
+            relation: "self"
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleName = this.handleName.bind(this)
+        this.handleRelation = this.handleRelation.bind(this)
+    }
+
+
+    handleSubmit(event){
+        this.props.closeModal()
+        event.preventDefault()
+        this.props.addRelation({
+            name: this.state.name,
+            relation: this.state.relation
+        })
+    }
+
+    handleName(event){
+        console.log(event.target.value)
+        this.setState({name:event.target.value})
+    }
+
+    handleRelation(event){
+        console.log(event.target.value)
+        this.setState({relation:event.target.value})
+    }
+
+
+    render() {
+        const showForm = this.props.showForm
+        const closeModal = this.props.closeModal
+
+        if (showForm) {
+            return (
+                <div className="modal">
+                    <div className="modal-content">
+                        <form onSubmit={this.handleSubmit}>
+                            <span className="close" onClick={closeModal}>&times;</span>
+                            <label>Name</label> <input type="text" onChange={this.handleName}/> <br/>
+                            <label>Relationship</label><RelationshipSelector onChange={this.handleRelation}/><br/>
+                            <button type="submit" value="submit" >Add</button>
+                        </form>
+                    </div>
+                </div>)
+        } else {
+            return null;
+        }
+    }
+}
+
+export default RelationshipForm
--- src/member/index.js
+++ src/member/index.js
@@ -1,8 +1,36 @@
 /**
  * Created by beggl on 10/20/2017.
  */
+import React from 'react'
+
+
 const Member = ({r}) =>
     <div>
-        <label>Name:</label> <span> {r.name}</span> <br/>
-        <label>Relation:</label> <span>{r.relation}</span> <br/>
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" style={{filter: 'drop-shadow( 5px 5px 5px black )'}}>
+            <g >
+                <circle cx="50%" cy="50%" r="50%" fill="#779ecb"  />
+                <text x="50%" y="50%" text-anchor="middle" >{r.name} </text>
+                <text x="50%" y="70%" text-anchor="middle" >{r.relation}</text>
+            </g>
+        </svg>
+        {/* <label>Name:</label> <span> {r.name}</span> <br/>
+         <label>Relation:</label> <span>{r.relation}</span> <br/>*/}
     </div>
+
+
+const MemberGrid = ({mems}) =>
+    <div>
+        {
+            mems.map(m => {
+                    return (
+                        <div key={m.name}>
+                            <Member r={m} />
+                            <br/>
+                        </div>)
+
+                }
+            )}
+    </div>
+
+
+export {Member, MemberGrid}
