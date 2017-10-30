56ca5dc2f2ec55d3efafcf3d7665fd4aa711b4ed
Brian Eggleston <bje301@gmail.com>
2017-10-29 23:30:10 -0400

    dirty commit

--- src/event/index.test.js
+++ src/event/index.test.js
@@ -4,6 +4,8 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import {Event} from './index'
+import {shallow, mount, render} from 'enzyme'
+
 
 
 it('Event component renders correct', ()=> {
@@ -17,3 +19,19 @@ it('Event component renders correct', ()=> {
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
 })
+
+describe('<Event>t', function () {
+    it('it renders the event with correct data',()=> {
+        const event = {
+            title: "Christmas of 1992",
+            date: new Date(1992, 11, 25),
+            description: "Christmas of 1992 was spent in New York City",
+        }
+        const wrapper = shallow(<Event e={event}/>)
+        expect(wrapper.text()).toContain('Christmas of 1992')
+
+    })
+
+})
+
+
--- src/member/index.js
+++ src/member/index.js
@@ -6,15 +6,8 @@ import PropTypes, {arrayOf, shape, string} from 'prop-types'
 
 const Member = ({r}) =>
     <div>
-        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" style={{filter: 'drop-shadow( 5px 5px 5px black )'}}>
-            <g >
-                <circle cx="50%" cy="50%" r="50%" fill="#779ecb"  />
-                <text x="50%" y="50%" text-anchor="middle" >{r.name} </text>
-                <text x="50%" y="70%" text-anchor="middle" >{r.relation}</text>
-            </g>
-        </svg>
-        {/* <label>Name:</label> <span> {r.name}</span> <br/>
-         <label>Relation:</label> <span>{r.relation}</span> <br/>*/}
+        <h1> {r.name} </h1>
+        <h2> {r.relation} </h2>
     </div>
 
 
