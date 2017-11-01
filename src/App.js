30fa6256688c56420cd1d162d68b824ac8498629
Brian Eggleston <bje301@gmail.com>
2017-10-31 21:30:45 -0400

    dirty commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './App.css'
-import {MemberGrid, MemberPage} from './member'
-import RelationshipForm from './relationship'
-import {EventForm} from './event'
-import withModal from './modal'
-import {Button, Menu} from 'semantic-ui-react'
+import {MemberPage} from './member'
+import {Button, Menu, Message, Icon} from 'semantic-ui-react'
 import {Link, Switch, Route} from 'react-router-dom'
+
+
+const Home = () =>
+    <div>
+        <h1> Welcome to Memories</h1>
+        <p> Memories allows you to document your family history. Get started below by adding family members or create a timeline. </p>
+    </div>
+
+const Timeline = () =>
+    <div>
+        <Message info>
+        <Message.Header> This page has not be implemented yet ! </Message.Header>
+        <Icon name='info'/>  Please check back soon
+        </Message>
+    </div>
 
 const Header = () =>
     <Menu inverted>
@@ -28,19 +40,6 @@ const Main = () =>
             <Route path='/timeline' component={Timeline}/>
         </Switch>
     </main>
-
-
-const Home = () =>
-    <div>
-        <h1> Welcome to Memories</h1>
-        <p> Memories allows you to document your family history. Get started below by adding family members or create a timeline. </p>
-    </div>
-
-const Timeline = () =>
-    <div>
-        <h1>Not implemented !</h1>
-    </div>
-
 
 const App = () =>
     <div>
